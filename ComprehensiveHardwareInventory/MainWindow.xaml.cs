d90dabda75e308c29531836cd92d4908fc2b8dd3
micheal.wei <micheal.wei@WEIHUIXIANG>
2018-08-31 17:07:24 +0800

    Add NPOI excel file support and basic xml operation. Also, some UI updates.

--- ComprehensiveHardwareInventory/MainWindow.xaml.cs
+++ ComprehensiveHardwareInventory/MainWindow.xaml.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.IO;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -12,6 +13,9 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.Xml.Serialization;
+using System.Xml;
+using System.Data;
 
 namespace ComprehensiveHardwareInventory
 {
@@ -20,20 +24,136 @@ namespace ComprehensiveHardwareInventory
     /// </summary>
     public partial class MainWindow : Window
     {
-        #region
+        #region fields
         private List<RowObject> ItemList;
+        private string persistenFileName = @"Files/TemplateConfig.Config";
+        private string TableToXMLFileName = @"TableConfig.xml";
+        private DataSet ds = new DataSet();
+        private ExcelHelper excelhelper;
         #endregion
         public MainWindow()
         {
             InitializeComponent();
-            ItemList = new List<RowObject>();
-            for(int i = 0; i < 1000; i++)
+            DataTable dt = new DataTable();
+            dt.Columns.Add("Channel", typeof(string));
+            dt.Columns.Add("Module", typeof(string));
+            dt.Columns.Add("Component", typeof(string));
+            dt.Columns.Add("Parameter", typeof(string));
+            dt.Columns.Add("Anonym", typeof(string));
+            dt.Columns.Add("PhysicalAddress", typeof(string));
+            dt.Columns.Add("Logic", typeof(string));
+            dt.Columns.Add("DateAdded", typeof(string));
+            dt.Columns["DateAdded"].DefaultValue = DateTime.Now.ToLocalTime();
+            dt.Columns.Add("Tag", typeof(string));
+            dt.Columns.Add("Comment", typeof(string));
+
+            //DataRow row = dt.NewRow();
+            //row["Channel"] = "AX0";
+            //row["Module"] = "System";
+            //row["Component"] = "CHEM1";
+            //row["Parameter"] = "TemperatureReading";
+            //row["PhysicalAddress"] = "CH #1";
+            //row["Logic"] = "/10";
+            ////row["DateAdded"] = DateTime.Now.ToLocalTime();
+            //row["Tag"] = "N/A";
+            //row["Comment"] = "N/A";
+            //dt.Rows.Add(row);
+            ds.Tables.Add(dt);
+            ParametersTable.ItemsSource = ds.Tables[0].DefaultView;
+            ParametersTable.LoadingRow += new EventHandler<DataGridRowEventArgs>(dataGrid_LoadingRow);
+            OverwriteXMLFile();
+        }
+
+
+        private void dataGrid_LoadingRow(object sender, DataGridRowEventArgs e)
+        {
+            e.Row.Header = e.Row.GetIndex() + 1;
+        }
+
+        private void dataGrid_RowEditEnding(object sender, DataGridRowEditEndingEventArgs e)
+        {
+           
+            if (this.ParametersTable.SelectedItem != null)
             {
-                ItemList.Add(new RowObject());
+                (sender as DataGrid).RowEditEnding -= dataGrid_RowEditEnding;
+                (sender as DataGrid).CommitEdit();
+                (sender as DataGrid).Items.Refresh();
+                (sender as DataGrid).RowEditEnding += dataGrid_RowEditEnding;
             }
 
-            RowTable.ItemsSource = ItemList;
+            DataRow dgRow = (DataRow)((DataRowView)e.Row.Item).Row;
+
         }
+
+        #region private methods
+
+        //Common helper methods
+        private void GetXMLHierachy()
+        {
+
+        }
+
+
+        private void OnClickGenerateXML(object sender, RoutedEventArgs e)
+        {
+            GetXMLHierachy();
+            //MoveMotorOperation(1, true);
+        }
+
+        private void OnClickSaveToXML(object sender, RoutedEventArgs e)
+        {
+            GetXMLHierachy();
+            //MoveMotorOperation(1, true);
+        }
+
+        private void OnClickSaveToExcel(object sender, RoutedEventArgs e)
+        {
+            string filename = String.Empty;
+            excelhelper = new ExcelHelper(filename);
+
+            GetXMLHierachy();
+            //MoveMotorOperation(1, true);
+        }
+        
+
+        // Manipulate XML
+        private void WriteXMLNode(string rootNode, double Xvalue, double Zvalue)
+        {
+            XmlDocument doc = new XmlDocument();
+            doc.Load(TableToXMLFileName);
+            XmlNode root = doc.DocumentElement;
+            XmlNode Position = root.SelectSingleNode(rootNode);
+            XmlNode XPosition = Position.SelectSingleNode("XPosition");
+            XPosition.InnerText = Xvalue.ToString();
+            XmlNode ZPosition = Position.SelectSingleNode("ZPosition");
+            ZPosition.InnerText = Zvalue.ToString();
+            doc.Save(persistenFileName);
+        }
+
+        private void OverwriteXMLFile()
+        {
+            XmlWriterSettings settings = new XmlWriterSettings();
+            settings.Indent = true;
+            using (XmlWriter writer = XmlWriter.Create(TableToXMLFileName, settings))
+            {
+                writer.WriteStartDocument();
+                writer.WriteStartElement("ToolControl");
+                writer.WriteStartElement("Group");
+                writer.WriteStartAttribute("Name");
+                writer.WriteValue("System");
+                writer.WriteEndElement();
+                writer.WriteEndElement();
+            }
+
+        }
+
+        private void WriteRowToXML(DataRow dr)
+        {
+            //WriteXMLNode();
+        }
+
+         
+        #endregion
     }
 
     public class RowObject
@@ -42,5 +162,37 @@ namespace ComprehensiveHardwareInventory
         public string NameFunction;
         public string Anonym;
         public string Logic;
+    }
+
+    [Serializable]
+    public class RbtPosition
+    {
+        [XmlElement(ElementName = "XPosition")]
+        public double x;
+        [XmlElement(ElementName = "ZPosition")]
+        public double z;
+        public RbtPosition(double X, double Z) { this.x = X; this.z = Z; }
+        public RbtPosition() { }
+    }
+
+    [Serializable]
+    public class RbtPositions
+    {
+        [XmlElement(ElementName = "LoadPosition")]
+        public RbtPosition LoadPosition { get; set; }
+        [XmlElement(ElementName = "UnloadPosition")]
+        public RbtPosition UnloadPosition { get; set; }
+        [XmlElement(ElementName = "SPMPosition")]
+        public RbtPosition SPMPosition { get; set; }
+        [XmlElement(ElementName = "QDRPosition")]
+        public RbtPosition QDRPosition { get; set; }
+        public RbtPositions() { }
+        public RbtPositions(RbtPosition LoadPos, RbtPosition UnloadPos, RbtPosition SPMPos, RbtPosition QDRPos)
+        {
+            LoadPosition = LoadPos;
+            UnloadPosition = UnloadPos;
+            SPMPosition = SPMPos;
+            QDRPosition = QDRPos;
+        }
     }
 }
---
+++ ComprehensiveHardwareInventory/Tools.cs
@@ -0,0 +1,192 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Data;
+using System.IO;
+using NPOI.SS.UserModel;
+using NPOI.XSSF.UserModel;
+using NPOI.HSSF.UserModel;
+using System.Collections;
+using System.Reflection;
+
+namespace ComprehensiveHardwareInventory
+{
+    public class Tools
+    {
+        #region 打开保存excel对话框返回文件名
+        public static string SaveExcelFileDialog()
+        {
+            var sfd = new Microsoft.Win32.SaveFileDialog()
+            {
+                DefaultExt = "xls",
+                Filter = "excel files(*.xls)|*.xls|All files(*.*)|*.*",
+                FilterIndex = 1
+            };
+
+            if (sfd.ShowDialog() != true)
+                return null;
+            return sfd.FileName;
+        }
+        #endregion
+        #region 打开excel对话框返回文件名
+        public static string OpenExcelFileDialog()
+        {
+            var ofd = new Microsoft.Win32.OpenFileDialog()
+            {
+                DefaultExt = "xls",
+                Filter = "excel files(*.xls)|*.xls|All files(*.*)|*.*",
+                FilterIndex = 1
+            };
+
+            if (ofd.ShowDialog() != true)
+                return null;
+            return ofd.FileName;
+        }
+        #endregion
+        #region 读excel
+        public static DataTable ImportExcelFile()
+        {
+            DataTable dt = new DataTable();
+
+            //打开excel对话框
+            var filepath = OpenExcelFileDialog();
+            if (filepath != null)
+            {
+
+                HSSFWorkbook hssfworkbook = null;
+                #region//初始化信息
+                try
+                {
+                    using (FileStream file = new FileStream(filepath, FileMode.Open, FileAccess.Read))
+                    {
+                        hssfworkbook = new HSSFWorkbook(file);
+                    }
+                }
+                catch (Exception e)
+                {
+                    throw e;
+                }
+                #endregion
+
+                var sheet = hssfworkbook.GetSheetAt(0);
+                System.Collections.IEnumerator rows = sheet.GetRowEnumerator();
+
+                for (int j = 0; j < (sheet.GetRow(0).LastCellNum); j++)
+                {
+                    dt.Columns.Add(Convert.ToChar(((int)'A') + j).ToString());
+                }
+                while (rows.MoveNext())
+                {
+                    HSSFRow row = (HSSFRow)rows.Current;
+                    DataRow dr = dt.NewRow();
+                    for (int i = 0; i < row.LastCellNum; i++)
+                    {
+                        var cell = row.GetCell(i);
+                        if (cell == null)
+                        {
+                            dr[i] = "";
+                        }
+                        else
+                        {
+                            if (cell.CellType == NPOI.SS.UserModel.CellType.Numeric)
+                            {
+                                if (HSSFDateUtil.IsCellDateFormatted(cell))
+                                {
+                                    dr[i] = cell.DateCellValue;
+                                }
+                                else
+                                {
+                                    dr[i] = cell.NumericCellValue;
+                                }
+                            }
+                            else if (cell.CellType == NPOI.SS.UserModel.CellType.Boolean)
+                            {
+                                dr[i] = cell.BooleanCellValue;
+                            }
+                            else
+                            {
+                                dr[i] = cell.StringCellValue;
+                            }
+                        }
+                    }
+                    dt.Rows.Add(dr);
+                }
+            }
+            return dt;
+        }
+        #endregion
+
+
+
+        #region list转datatable
+        public static DataTable ListToDataTable<T>(IEnumerable<T> c)
+        {
+            var props = typeof(T).GetProperties();
+            var dt = new DataTable();
+            dt.Columns.AddRange(props.Select(p => new DataColumn(p.Name, p.PropertyType)).ToArray());
+            if (c.Count() > 0)
+            {
+                for (int i = 0; i < c.Count(); i++)
+                {
+                    ArrayList tempList = new ArrayList();
+                    foreach (PropertyInfo item in props)
+                    {
+                        object obj = item.GetValue(c.ElementAt(i), null);
+                        tempList.Add(obj);
+                    }
+                    dt.LoadDataRow(tempList.ToArray(), true);
+                }
+            }
+            return dt;
+        }
+        #endregion
+        #region 写入excel
+        public static bool WriteExcel<T>(IList<T> list)
+        {
+
+            //打开保存excel对话框
+            var filepath = SaveExcelFileDialog();
+            if (filepath == null)
+                return false;
+
+            var dt = ListToDataTable<T>(list);
+
+            if (!string.IsNullOrEmpty(filepath) && null != dt && dt.Rows.Count > 0)
+            {
+                NPOI.HSSF.UserModel.HSSFWorkbook book = new NPOI.HSSF.UserModel.HSSFWorkbook();
+                NPOI.SS.UserModel.ISheet sheet = book.CreateSheet("Sheet1");
+
+                NPOI.SS.UserModel.IRow row = sheet.CreateRow(0);
+                for (int i = 0; i < dt.Columns.Count; i++)
+                {
+                    row.CreateCell(i).SetCellValue(dt.Columns[i].ColumnName);
+                }
+                for (int i = 0; i < dt.Rows.Count; i++)
+                {
+                    NPOI.SS.UserModel.IRow row2 = sheet.CreateRow(i + 1);
+                    for (int j = 0; j < dt.Columns.Count; j++)
+                    {
+                        row2.CreateCell(j).SetCellValue(Convert.ToString(dt.Rows[i][j]));
+                    }
+                }
+                // 写入到客户端  
+                using (System.IO.MemoryStream ms = new System.IO.MemoryStream())
+                {
+                    book.Write(ms);
+                    using (FileStream fs = new FileStream(filepath, FileMode.Create, FileAccess.Write))
+                    {
+                        byte[] data = ms.ToArray();
+                        fs.Write(data, 0, data.Length);
+                        fs.Flush();
+                    }
+                    book = null;
+                }
+            }
+            return true;
+        }
+        #endregion
+    }
+}
